3154be0c936fcf41b4bf62642cd8c0b7d700b213
siddharthM1 <siddharth.gla1_cs17@gla.ac.in>
2019-11-20 12:47:37 +0530

    mini poject

---
+++ php-includes/footer.inc.php
@@ -0,0 +1,10 @@
+    
+        </div>
+        
+        <footer>
+            <p>Instant update database project by <a href="siddharth.gla1_cs17@gla.ac.in">siddharth.gla1_cs17@gla.ac.in</a></p>
+        </footer>
+        
+    </body>
+   
+</html>
